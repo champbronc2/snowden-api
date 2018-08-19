c882fffeffd9f059eee53b2a5ff0cfb8f43c0f68
champbronc2 <jadmubaslat@gmail.com>
2018-08-19 09:29:20 -0700

    new feature confirm convo

--- index.php
+++ index.php
@@ -61,7 +61,27 @@ if($_GET['type']=="users"){
 		$eth_address = $data['eth_address'];
 		$query="INSERT INTO `users`(`name`,`profile_pic_url`,`eth_address`) VALUES ('".$name."','".$profile_pic_url."','".$eth_address."')";
 		$go=mysql_query($query) or die(mysql_error());
-		echo("record created");
+		$last = mysql_insert_id();
+		//fetch a specific record
+			$query="SELECT CONCAT('[', better_result, ']') AS best_result FROM
+	(
+	SELECT GROUP_CONCAT('{', my_json, '}' SEPARATOR ',') AS better_result FROM
+	(
+	  SELECT 
+		CONCAT
+		(
+		  '\"id\":'   , '\"', id   , '\"', ',' 
+		  '\"name\":', '\"', name, '\"', ','
+		  '\"eth_address\":', '\"', eth_address, '\"', ','
+		  '\"profile_pic_url\":', '\"', profile_pic_url, '\"'
+		) AS my_json
+	  FROM users
+	  WHERE id = ".$last."
+	) AS more_json
+	) AS yet_more_json;";
+			$go=mysql_query($query) or die(mysql_error());
+			$result = mysql_result($go, 0);
+			echo($result);
 	} else {
 		//GET logic
 		if (isset($_GET['id'])){
