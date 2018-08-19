bcf8b07b2495b2b0a17ba0ae535b95ba7426c389
champbronc2 <jadmubaslat@gmail.com>
2018-08-18 22:56:57 -0700

    maybe htaccess issues

--- web/index.php
+++ web/index.php
@@ -1,7 +1,4 @@
 <?PHP
-
-echo("PHP is working");
-var_dump( realpath( __DIR__ . '/../..' ) );
 $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server   = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
@@ -32,75 +29,227 @@ $db['default'] = array(
     'failover' => array(),
     'save_queries' => TRUE
 );
-/* Below is deprecated
-$hostname = "localhost"; // eg. mysql.yourdomain.com (unique)
-$username = "root";   // the username specified when setting-up the database
-$password = "";   // the password specified when setting-up the database
-$database = "snowden";   // the database name chosen when setting-up the database (unique)
-$tablename = "users"; //name of table to connect to
-*/
-error_reporting(1);
+// connect to the database
+error_reporting(0);
 $link = mysql_connect($cleardb_server,$cleardb_username,$cleardb_password);
 		@mysql_select_db($cleardb_db) or die( "Unable to select database!");
 		mysql_query("SET NAMES 'utf8'");
 	mysql_query("SET CHARACTER SET utf8");
 	mysql_query("SET COLLATION_CONNECTION = 'utf8_unicode_ci'");
-	
+
+if($_GET['type']=="users"){	
 	
 	$data = json_decode(file_get_contents('php://input'), true);
-//if post
-if($data!=""){
-	//POST logic
-	$name = $data['name'];
-	$profile_pic_url = $data['profile_pic_url'];
-	$eth_address = $data['eth_address'];
-	$query="INSERT INTO `users`(`name`,`profile_pic_url`,`eth_address`) VALUES ('".$name."','".$profile_pic_url."','".$eth_address."')";
-	$go=mysql_query($query) or die(mysql_error());
-	echo("record created");
-} else {
-	//GET logic
-	if (isset($_GET['id'])){
-		//fetch a specific record
-		$query="SELECT CONCAT('[', better_result, ']') AS best_result FROM
-(
-SELECT GROUP_CONCAT('{', my_json, '}' SEPARATOR ',') AS better_result FROM
-(
-  SELECT 
-    CONCAT
-    (
-      '\"id\":'   , '\"', id   , '\"', ',' 
-      '\"name\":', '\"', name, '\"', ','
-      '\"eth_address\":', '\"', eth_address, '\"', ','
-      '\"profile_pic_url\":', '\"', profile_pic_url, '\"'
-    ) AS my_json
-  FROM users
-  WHERE id = ".$_GET['id']."
-) AS more_json
-) AS yet_more_json;";
+	//if post
+	if($data!=""){
+		//POST logic
+		$name = $data['name'];
+		$profile_pic_url = $data['profile_pic_url'];
+		$eth_address = $data['eth_address'];
+		$query="INSERT INTO `users`(`name`,`profile_pic_url`,`eth_address`) VALUES ('".$name."','".$profile_pic_url."','".$eth_address."')";
 		$go=mysql_query($query) or die(mysql_error());
-		$result = mysql_result($go, 0);
-		echo($result);
+		echo("record created");
 	} else {
-		//fetch all records
-		$query="SELECT CONCAT('[', better_result, ']') AS best_result FROM
-(
-SELECT GROUP_CONCAT('{', my_json, '}' SEPARATOR ',') AS better_result FROM
-(
-  SELECT 
-    CONCAT
-    (
-      '\"id\":'   , '\"', id   , '\"', ',' 
-      '\"name\":', '\"', name, '\"', ','
-      '\"eth_address\":', '\"', eth_address, '\"', ','
-      '\"profile_pic_url\":', '\"', profile_pic_url, '\"'
-    ) AS my_json
-  FROM users
-) AS more_json
-) AS yet_more_json;";
-		$go=mysql_query($query) or die(mysql_error());
-		$result = mysql_result($go, 0);
-		echo($result);
+		//GET logic
+		if (isset($_GET['id'])){
+			//fetch a specific record
+			$query="SELECT CONCAT('[', better_result, ']') AS best_result FROM
+	(
+	SELECT GROUP_CONCAT('{', my_json, '}' SEPARATOR ',') AS better_result FROM
+	(
+	  SELECT 
+		CONCAT
+		(
+		  '\"id\":'   , '\"', id   , '\"', ',' 
+		  '\"name\":', '\"', name, '\"', ','
+		  '\"eth_address\":', '\"', eth_address, '\"', ','
+		  '\"profile_pic_url\":', '\"', profile_pic_url, '\"'
+		) AS my_json
+	  FROM users
+	  WHERE id = ".$_GET['id']."
+	) AS more_json
+	) AS yet_more_json;";
+			$go=mysql_query($query) or die(mysql_error());
+			$result = mysql_result($go, 0);
+			echo($result);
+		} else {
+			//fetch all records
+			$query="SELECT CONCAT('[', better_result, ']') AS best_result FROM
+	(
+	SELECT GROUP_CONCAT('{', my_json, '}' SEPARATOR ',') AS better_result FROM
+	(
+	  SELECT 
+		CONCAT
+		(
+		  '\"id\":'   , '\"', id   , '\"', ',' 
+		  '\"name\":', '\"', name, '\"', ','
+		  '\"eth_address\":', '\"', eth_address, '\"', ','
+		  '\"profile_pic_url\":', '\"', profile_pic_url, '\"'
+		) AS my_json
+	  FROM users
+	) AS more_json
+	) AS yet_more_json;";
+			$go=mysql_query($query) or die(mysql_error());
+			$result = mysql_result($go, 0);
+			echo($result);
+		}
 	}
+} else if($_GET['type']=="messages"){
+		$data = json_decode(file_get_contents('php://input'), true);
+	//if post
+	if($data!=""){
+		//POST logic
+		$content = $data['content'];
+		$user_id = $data['user_id'];
+		$conversation_id = $data['conversation_id'];
+		$query="INSERT INTO `messages`(`content`,`user_id`,`conversation_id`) VALUES ('".$content."','".$user_id."','".$conversation_id."')";
+		$go=mysql_query($query) or die(mysql_error());
+		echo("record created");
+	} else {
+		//GET logic
+		if (isset($_GET['id'])){
+			//fetch a specific record
+			$query="SELECT CONCAT('[', better_result, ']') AS best_result FROM
+	(
+	SELECT GROUP_CONCAT('{', my_json, '}' SEPARATOR ',') AS better_result FROM
+	(
+	  SELECT 
+		CONCAT
+		(
+		  '\"id\":'   , '\"', id   , '\"', ',' 
+		  '\"created\":', '\"', created, '\"', ','
+		  '\"content\":', '\"', content, '\"', ','
+		  '\"user_id\":', '\"', user_id, '\"', ','
+		  '\"conversation_id\":', '\"', conversation_id, '\"', ','
+		  '\"reported\":', '\"', reported, '\"'
+		) AS my_json
+	  FROM messages
+	  WHERE id = ".$_GET['id']."
+	) AS more_json
+	) AS yet_more_json;";
+			$go=mysql_query($query) or die(mysql_error());
+			$result = mysql_result($go, 0);
+			echo($result);
+		} else {
+			//fetch all records
+			$query="SELECT CONCAT('[', better_result, ']') AS best_result FROM
+	(
+	SELECT GROUP_CONCAT('{', my_json, '}' SEPARATOR ',') AS better_result FROM
+	(
+	  SELECT 
+		CONCAT
+		(
+		 '\"id\":'   , '\"', id   , '\"', ',' 
+		  '\"created\":', '\"', created, '\"', ','
+		  '\"content\":', '\"', content, '\"', ','
+		  '\"user_id\":', '\"', user_id, '\"', ','
+		  '\"conversation_id\":', '\"', conversation_id, '\"', ','
+		  '\"reported\":', '\"', reported, '\"'
+		) AS my_json
+	  FROM messages
+	) AS more_json
+	) AS yet_more_json;";
+			$go=mysql_query($query) or die(mysql_error());
+			$result = mysql_result($go, 0);
+			echo($result);
+		}
+	}
+} else if($_GET['type']=="conversations"){
+		$data = json_decode(file_get_contents('php://input'), true);
+	//if post
+	if($data!=""){
+		//POST logic
+		$name = $data['name'];
+		$expiration_date = $data['expiration_date'];
+		$start_distribution = $data['start_distribution'];
+		$complete_distribution = $data['complete_distribution'];
+		$user_id = $data['user_id'];
+		$recipient_id = $data['recipient_id'];
+		$amount = $data['amount'];
+		$query="INSERT INTO `conversations`(`name`,`expiration_date`,`start_distribution`,`complete_distribution`,`user_id`,`recipient_id`,`amount`) VALUES ('".$name."','".$expiration_date."','".$start_distribution."','".$complete_distribution."','".$user_id."','".$recipient_id."','".$amount."')";
+		$go=mysql_query($query) or die(mysql_error());
+		echo("record created");
+	} else {
+		//GET logic
+		if (isset($_GET['id'])){
+			//fetch a specific record
+			$query="SELECT CONCAT('[', better_result, '') AS best_result FROM
+	(
+	SELECT GROUP_CONCAT('{', my_json, ',\"messages\":' SEPARATOR ',') AS better_result FROM
+	(
+	  SELECT 
+		CONCAT
+		(
+		 '\"id\":'   , '\"', id   , '\"', ',' 
+		  '\"created\":', '\"', created, '\"', ','
+		  '\"status\":', '\"', status, '\"', ','
+		  '\"name\":', '\"', name, '\"', ','
+		  '\"expiration_date\":', '\"', expiration_date, '\"', ','
+		  '\"complete_distribution\":', '\"', complete_distribution, '\"', ','
+		  '\"user_id\":', '\"', user_id, '\"', ','
+		  '\"recipient_id\":', '\"', recipient_id, '\"', ','
+		  '\"amount\":', '\"', amount, '\"', ','
+		  '\"start_distribution\":', '\"', start_distribution, '\"'
+		) AS my_json
+	  FROM conversations
+	  WHERE id = ".$_GET['id']."
+	) AS more_json
+	) AS yet_more_json;";
+			$go=mysql_query($query) or die(mysql_error());
+			$result = mysql_result($go, 0);
+			echo($result);
+			//fetch and append the messages from that conversation next
+	$query="SELECT CONCAT('[', better_result, ']}]') AS best_result FROM
+	(
+		SELECT GROUP_CONCAT('{', my_json, '}' SEPARATOR ',') AS better_result FROM
+	(
+	  SELECT 
+		CONCAT
+		(
+		'\"id\":'   , '\"', id   , '\"', ',' 
+		  '\"created\":', '\"', created, '\"', ','
+		  '\"content\":', '\"', content, '\"', ','
+		  '\"user_id\":', '\"', user_id, '\"', ','
+		  '\"conversation_id\":', '\"', conversation_id, '\"', ','
+		  '\"reported\":', '\"', reported, '\"'
+		) AS my_json
+	  FROM messages
+	  WHERE conversation_id = ".$_GET['id']."
+	) AS more_json
+	) AS yet_more_json;";
+			$go=mysql_query($query) or die(mysql_error());
+			$result = mysql_result($go, 0);
+			echo($result);
+		} else {
+			//fetch all records
+			$query="SELECT CONCAT('[', better_result, ']') AS best_result FROM
+	(
+	SELECT GROUP_CONCAT('{', my_json, '}' SEPARATOR ',') AS better_result FROM
+	(
+	  SELECT 
+		CONCAT
+		(
+		 '\"id\":'   , '\"', id   , '\"', ',' 
+		  '\"created\":', '\"', created, '\"', ','
+		  '\"status\":', '\"', status, '\"', ','
+		  '\"name\":', '\"', name, '\"', ','
+		  '\"expiration_date\":', '\"', expiration_date, '\"', ','
+		  '\"complete_distribution\":', '\"', complete_distribution, '\"', ','
+		  '\"user_id\":', '\"', user_id, '\"', ','
+		  '\"recipient_id\":', '\"', recipient_id, '\"', ','
+		  '\"amount\":', '\"', amount, '\"', ','
+		  '\"start_distribution\":', '\"', start_distribution, '\"'
+		) AS my_json
+	  FROM conversations
+	) AS more_json
+	) AS yet_more_json;";
+			$go=mysql_query($query) or die(mysql_error());
+			$result = mysql_result($go, 0);
+			echo($result);
+		}
+	}
+} else {
+	echo("Please specify a type of users messages or conversations");
 }
 
 ?>
