eb1886500105c8a1a76eca6f97f62c6807d58e60
champbronc2 <jadmubaslat@gmail.com>
2018-08-19 09:10:14 -0700

    new feature confirm convo

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ $link = mysql_connect($cleardb_server,$cleardb_username,$cleardb_password);
 	mysql_query("SET COLLATION_CONNECTION = 'utf8_unicode_ci'");
 	
 	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: *");
 	if (file_exists("src/autoloader.php")) {
 		//require "src/autoloader.php";
 		//require "vendor/autoload.php";
