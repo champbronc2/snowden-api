e3768e9a290ae686930a7f7beeccccab73424df3
champbronc2 <jadmubaslat@gmail.com>
2018-08-19 00:52:52 -0700

    new feature confirm convo

--- web/index.php
+++ web/index.php
@@ -38,7 +38,6 @@ $link = mysql_connect($cleardb_server,$cleardb_username,$cleardb_password);
 	mysql_query("SET COLLATION_CONNECTION = 'utf8_unicode_ci'");
 	
 	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Headers: *");
 
 if($_GET['type']=="users"){	
 	
